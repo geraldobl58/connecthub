ec9e380f45f2a81c3a4b5318717199078c41f96e
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-25 10:26:51 -0300

    feat: Initialize application modules and configure global settings

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Connecthub API')
+    .setDescription('The Connecthub API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
