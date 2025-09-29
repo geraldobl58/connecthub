909bbf3ff42345667730b1c459f0af12842cf1cc
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-29 10:48:59 -0300

    fix: update Logo component to streamline image import and improve code readability

--- apps/web/src/components/logo.tsx
+++ apps/web/src/components/logo.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Logo = () => {
   return (
     <Link passHref href="/">
-      <Image
-        src="/assets/images/logo.svg"
-        alt="Connecthub"
-        width={30}
-        height={30}
-      />
+      <Image src="/assets/images/logo.svg" alt="Logo" width={30} height={30} />
     </Link>
   );
 };
