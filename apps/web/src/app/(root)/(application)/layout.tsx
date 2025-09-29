42feb2b39a53e26289ec52965be415276697cd21
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-29 10:47:04 -0300

    feat: create HomePage component for client-side routing based on authentication status

--- apps/web/src/app/(root)/(application)/layout.tsx
+++ apps/web/src/app/(root)/(application)/layout.tsx
@@ -1,7 +1,16 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 export default function ApplicationLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="flex items-center justify-center">{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarTrigger />
+      <main className="flex items-center justify-center">{children}</main>
+    </SidebarProvider>
+  );
 }
