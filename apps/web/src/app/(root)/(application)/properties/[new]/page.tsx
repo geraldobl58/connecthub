ba5766f26a74c28bf75e0de5a1fa061afec24777
Geraldo Luiz <geraldobl58@gmail.com>
2025-10-03 09:06:14 -0300

    feat: replace PropertiesForm with PropertyFormStepper in the new property creation page for improved user experience

--- apps/web/src/app/(root)/(application)/properties/[new]/page.tsx
+++ apps/web/src/app/(root)/(application)/properties/[new]/page.tsx
@@ -1,18 +1,17 @@
 import { Header } from "@/components/header";
-import { PropertiesForm } from "../components/properties-form";
+import { PropertyFormStepper } from "@/components/property-form-stepper";
 
 const NewPropertyPage = () => {
   return (
-    <div className="space-y-6">
+    <>
       <Header
         title="Nova Propriedade"
         description="Crie uma nova propriedade no sistema"
       />
-
       <div className="container mx-auto p-6 space-y-6">
-        <PropertiesForm mode="create" />
+        <PropertyFormStepper mode="create" />
       </div>
-    </div>
+    </>
   );
 };
 
