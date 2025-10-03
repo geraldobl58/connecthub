b487ad7ed5392cc2aea76ce2dc9a5e33a7fc0ddf
Geraldo Luiz <geraldobl58@gmail.com>
2025-10-03 09:06:56 -0300

    feat: create index file for property steps to streamline exports of BasicInfoStep, CharacteristicsStep, AddressStep, and MediaStep components

---
+++ apps/web/src/components/property-steps/index.ts
@@ -0,0 +1,4 @@
+export { BasicInfoStep } from "./basic-info-step";
+export { CharacteristicsStep } from "./characteristics-step";
+export { AddressStep } from "./address-step";
+export { MediaStep } from "./media-step";
